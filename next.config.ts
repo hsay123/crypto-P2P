8d7be22fedb869b5d18520562c5b3b29fcec57ca
hsay123 <yashlandge150@gmail.com>
2026-03-07 01:44:38 +0530

    fix: remove deprecated turbo from experimental config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.37"],
   experimental: {
-    turbo: {
-      root: __dirname,
-    },
   },
 };
 
